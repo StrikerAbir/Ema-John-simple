721b1b679ec4005fac28de585f3cfd813564343a
Abir Hasan <cse.170201063@gmail.com>
2022-09-26 18:44:09 +0600

    49.4 Setting the cart data on the local storage for future retrieve

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,21 +4,25 @@ import './Cart.css'
 const Cart = ({ cart }) => {
     // way 1 calculate total
     // let total = 0;
+    // let shipping = 0;
     // for (const product of cart) {
     //     total = total + product.price;
+    // shipping = shipping + product.shipping;
     // }
     // way 2 
-    const total = cart.reduce((previous, current) => previous + current.price, 0)
+    const total = cart.reduce((previous, current) => previous + current.price, 0);
+    const shipping = cart.reduce((previous, current) => previous + current.shipping, 0);
     
-    const tax = parseFloat(total * 0.1).toFixed(2);
+    const tax = (total * 0.1).toFixed(2);
+    const grandTotal = total + shipping + parseFloat(tax);
     return (
       <div className='cart'>
         <h1>Order Summery</h1>
         <p>Selected Items: {cart.length}</p>
         <p>Total Prize: $ {total}</p>
-        <p>Total Shipping: {tax}</p>
-        <p>Tax: {cart.length}</p>
-        <h4>Grand Total: {cart.length}</h4>
+        <p>Total Shipping: {shipping}</p>
+        <p>Tax: {tax}</p>
+        <h4>Grand Total: {grandTotal.toFixed(2)}</h4>
       </div>
     );
 };
